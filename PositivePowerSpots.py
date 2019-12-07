ab55d18a7fd9a02b230931501a4d4081015d4c67
Alan Aneas <aaneas@emi.u-bordeaux1.fr>
2019-12-07 15:21:04 +0100

    PositivePS fixed

--- PositivePowerSpots.py
+++ PositivePowerSpots.py
@@ -1,30 +1,32 @@
-import abs from math
+# y being the abscissa, and x being the ordinate, even though it's unusual, in order to have the user write the abscissa before and the ordinate after
+def PositivePowerSpots(powerSpots, y, x, length = 10):
 
-def PositivePowerSpots(powerSpots, x, y, length = 10):
-
-    if x != y || x != 0 || x != length - 1:
+    if (y != 0 and y != length - 1) or (x != 0 and x != length - 1):
         return
+
     
     if x == 0 :
-        powerSpots[1,y] = abs(powerSpots[1,y])
-
+        powerSpots[1][y] = abs(powerSpots[1][y])
+        
     elif x == length - 1 :
-        powerSpots[x-1,y] = abs(powerSpots[x-1,y])
+        powerSpots[x-1][y] = abs(powerSpots[x-1][y])
 
     if y == 0 :
-        powerSpots[x,1] = abs(powerSpots[x,1])
+        print("d")
+        powerSpots[x][1] = abs(powerSpots[x][1])
 
     elif y == length - 1 :
-        powerSpots[x,y-1] = abs(powerSpots[x,y-1])
+        powerSpots[x][y-1] = abs(powerSpots[x][y-1])
 
-    if x == 0 && y == 0 :
-        powerSpots[1,1] = abs(powerSpots[1,1])
+    if x == 0 and y == 0 :
+        powerSpots[1][1] = abs(powerSpots[1][1])
 
-    elif x == 0 && y == length - 1 :
-        powerSpots[1,y-1] = abs(powerSpots[1,y-1])
+    elif x == 0 and y == length - 1 :
+        powerSpots[1][y-1] = abs(powerSpots[1][y-1])
 
-    elif y == 0 && x == length - 1 :
-        powerSpots[x-1,1] = abs(powerSpots[x-1,1])
+    elif y == 0 and x == length - 1 :
+        powerSpots[x-1][1] = abs(powerSpots[x-1][1])
 
-    elif x == length-1 && y == length - 1 :
-        powerSpots[x-1,y-1] = abs(powerSpots[x-1,y-1])
+    elif x == length-1 and y == length - 1 :
+        powerSpots[x-1][y-1] = abs(powerSpots[x-1][y-1])
+
