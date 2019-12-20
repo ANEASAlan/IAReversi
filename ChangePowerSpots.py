76ace286230e0c1beaf279cd59aded7b48356632
Alan Aneas <aaneas@emi.u-bordeaux1.fr>
2019-12-20 16:08:13 +0100

    Function added in ChangePowerSpots.py

--- ChangePowerSpots.py
+++ ChangePowerSpots.py
@@ -14,7 +14,7 @@ def ChangePowerSpots(powerSpots, x, y, value, width = 10):
 
     subChange(powerSpots, x, y, value, width)
 
-    if(x != y):
+    if(x != y and y != width - x - 1 and x != width - y - 1):
         subChange(powerSpots, y, x, value, width)
 
 def subAdd(powerSpots, x, y, value, width = 10):
@@ -30,8 +30,19 @@ def subAdd(powerSpots, x, y, value, width = 10):
     powerSpots[x][y] += value
 
 def AddPowerSpots(powerSpots, x, y, value, width = 10):
-
     subAdd(powerSpots, x, y, value, width)
 
-    if(x != y):
+    if(x != y and y != width - x - 1 and x != width - y - 1):
         subAdd(powerSpots, y, x, value, width)
+
+def AddForEntireTable(powerSpots, table):
+    l = len(table) # Suposedly a table of size 15 in order to change the entire table
+    x = 0
+    y = 0
+    for i in range(l):
+        AddPowerSpots(powerSpots, x, y, table[i])
+        if x == y:
+            x = 0
+            y += 1
+        else:
+            x += 1
